b28509527e2b376e1160ce66b92e721a1470255b
jonksc <shaocherngkong@gmail.com>
2021-06-01 23:18:56 +0200

    Get errand data via props from the main page

--- src/components/UI/ErrandCard.js
+++ src/components/UI/ErrandCard.js
@@ -1,13 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import DogWalkingImg from '../../images/dog_walking.jpg';
 
-const ErrandCard = () => {
+const ErrandCard = (props) => {
   return (
     <div className="card col-8 col-md-7 col-lg-6 col-xl-5 col-xxl-4 mx-auto mt-4">
-      <img src={DogWalkingImg} className="card-img card-img-top mx-auto pt-3" alt="dog-walking" />
+      <img src={props.image} className="card-img card-img-top mx-auto pt-3" alt="dog-walking" />
       <div className="card-body">
-        <h5 className="card-title">Dog walking</h5>
-        <p className="card-text">Looking for a dog lover to walk my puppy 🐶</p>
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.description}</p>
         <a href="#" className="btn btn-dark">More details</a>
       </div>
     </div>
