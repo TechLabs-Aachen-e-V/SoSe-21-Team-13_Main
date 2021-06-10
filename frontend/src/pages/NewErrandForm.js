7417d52f67280ae3cc0bdcb46e594782aee8e635
jonksc <shaocherngkong@gmail.com>
2021-06-10 20:24:02 +0200

    Get user input with event object instead of useRef hook

--- frontend/src/pages/NewErrandForm.js
+++ frontend/src/pages/NewErrandForm.js
@@ -1,31 +1,20 @@
-import React, { Fragment, useRef } from 'react';
+import React, { Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const NewErrandForm = () => {
-
-  const titleInputRef = useRef();
-  const descriptionInputRef = useRef();
-  const locationInputRef = useRef();
-  const compensationInputRef = useRef();
-  const dateDueInputRef = useRef();
-  const timeDueInputRef = useRef();
-  const categoryInputRef = useRef();
-  const imageInputRef = useRef();
-
   const history = useHistory();
 
   const submitHandler = async (event) => {
     event.preventDefault();
-    
     const errandData = {
-      title: titleInputRef.current.value,
-      description: descriptionInputRef.current.value,
-      location: locationInputRef.current.value,
-      compensation: compensationInputRef.current.value,
-      dateDue: dateDueInputRef.current.value,
-      timeDue: timeDueInputRef.current.value,
-      category: categoryInputRef.current.value,
-      imageUrl: imageInputRef.current.value
+      title: event.target['errand-title'].value,
+      description: event.target['errand-description'].value,
+      location: event.target['errand-location'].value,
+      compensation: event.target['errand-compensation'].value,
+      dateDue: event.target['date-due'].value,
+      timeDue: event.target['time-due'].value,
+      category: event.target['category'].value,
+      imageUrl: event.target['errand-image'].value
     };
 
     await fetch(
@@ -47,31 +36,31 @@ const NewErrandForm = () => {
       <form className='col-10 col-md-7 col-lg-6 col-xl-5 col-xxl-4 mx-auto pt-5' onSubmit={submitHandler} >
         <div className='form-group'>
           <label htmlFor='errand-title'>Errand title</label>
-          <input type='text' className='form-control' id='errand-title' placeholder='Dog-walking' ref={titleInputRef} required autoFocus />
+          <input type='text' className='form-control' id='errand-title' placeholder='Dog-walking' required autoFocus />
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='errand-description' >Description</label>
-          <textarea className='form-control' id='errand-description' rows='3' placeholder='Looking for a dog lover to walk my puppy 🐶' ref={descriptionInputRef} required></textarea>
+          <textarea className='form-control' id='errand-description' rows='3' placeholder='Looking for a dog lover to walk my puppy 🐶' required></textarea>
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='errand-location'>Errand location</label>
-          <input type='text' className='form-control' id='errand-location' placeholder='Aachen-Mitte' ref={locationInputRef} />
+          <input type='text' className='form-control' id='errand-location' placeholder='Aachen-Mitte' />
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='errand-compensation'>Errand compensation (EUR)</label>
-          <input type='number' step='.1' className='form-control' id='errand-compensation' placeholder='5.5' ref={compensationInputRef} required />
+          <input type='number' step='.1' className='form-control' id='errand-compensation' placeholder='5.5' required />
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='date-due'>Date due</label>
-          <input type='date' className='form-control' id='date-due' ref={dateDueInputRef} required />
+          <input type='date' className='form-control' id='date-due' required />
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='time-due'>Time due</label>
-          <input type='time' className='form-control' id='time-due' ref={timeDueInputRef} />
+          <input type='time' className='form-control' id='time-due' />
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='category'>Category</label>
-          <select className='form-control' id='category' ref={categoryInputRef}>
+          <select className='form-control' id='category'>
             <option>Household</option>
             <option>Pet</option>
             <option>Delivery</option>
@@ -80,7 +69,7 @@ const NewErrandForm = () => {
         </div>
         <div className='form-group pt-2'>
           <label htmlFor='errand-image'>Image URL</label>
-          <input type='text' className='form-control' id='errand-image' placeholder='https://source.unsplash.com/random' ref={imageInputRef} />
+          <input type='text' className='form-control' id='errand-image' placeholder='https://source.unsplash.com/random' />
         </div>
         {/* <div className="form-group pt-2">
           <label htmlFor="img-upload">Upload an image:</label>
