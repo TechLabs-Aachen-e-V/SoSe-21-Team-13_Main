101ca5af26e95494df18c8ef34509428e2d4eb8a
jonksc <shaocherngkong@gmail.com>
2021-06-13 22:03:35 +0200

    Update login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,26 +1,52 @@
 import React, { Fragment } from 'react';
+import { useHistory } from 'react-router-dom';
 import logo from '../images/helpify_logo.png';
 
 const Login = () => {
+  const history = useHistory();
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const username = event.target['username'].value;
+    const password = event.target['password'].value;
+
+    try {
+      const user = {
+        username,
+        password
+      };
+
+      await fetch(
+        '/login',
+        {
+          method: 'POST',
+          body: JSON.stringify(user),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+
+      console.log('logged in');
+      history.replace('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Fragment>
-      <form className='container col-md-7 col-lg-6 col-xl-5 col-xxl-4 mt-5'>
+      <form className='container col-md-7 col-lg-6 col-xl-5 col-xxl-4 mt-5' onSubmit={submitHandler}>
         <img className='mb-4 text-center d-block mx-auto' src={logo} alt='' width='200' />
         <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
 
         <div className='form-floating'>
-          <input type='email' className='form-control mb-2' id='email' placeholder='name@example.com' />
-          <label htmlFor='email'>Email address</label>
+          <input type='text' className='form-control mb-2' id='username' placeholder='name@example.com' />
+          <label htmlFor='username'>Username</label>
         </div>
         <div className='form-floating'>
           <input type='password' className='form-control mb-2' id='password' placeholder='Password' />
           <label htmlFor='password'>Password</label>
-        </div>
-
-        <div className='checkbox mb-3'>
-          <label>
-            <input type='checkbox' value='remember-me' /> Remember me
-          </label>
         </div>
         <button className='w-100 btn btn-lg btn-dark' type='submit'>Sign in</button>
         <p className='mt-5 mb-3 text-muted text-center'>© 2021</p>
