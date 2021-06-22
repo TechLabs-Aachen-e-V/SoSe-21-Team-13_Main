0f64d18977b0290a9971661dd68ba51c2517a1b1
Dilara <dilara.cekic@alumni.fh-aachen.de>
2021-06-22 19:19:00 +0200

    home page styling edited

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,13 +2,22 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import ErrandCard from '../components/UI/ErrandCard';
 import useFetchGet from '../hooks/useFetchGet';
+import illu from '../images/illu_1.png';
 
 const MainPage = () => {
   const [ isLoading, data ] = useFetchGet('/errands');
 
   return (
     <Fragment>
+      <div className='intro-container'>
+      <img className='main-illu' src={illu} alt='' width='40%'></img>
+      <div className='intro-text'>
+       <h1>Welcome to Helpify</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+      </div>
+      </div>
       {!isLoading ? (
+        <div className='container'>
         <ul>
           {data.map(errand => {
             return (
@@ -26,6 +35,7 @@ const MainPage = () => {
             );
           })}
         </ul>
+        </div>
       ) : (<p>Loading...</p>)}
       
       <div className='pt-4 mb-5' align='center'>
