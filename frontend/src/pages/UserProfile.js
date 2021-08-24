c61ebaee64b0d9e8e863aab2f652f5680c9042e9
jonksc <shaocherngkong@gmail.com>
2021-08-24 19:59:34 +0200

    Show booked and unbooked errands on profile page

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -24,9 +24,11 @@ const UserProfile = () => {
         </ul>
       ) : (<p>Loading...</p>)}
 
+      <h2 className='px-4'>Unbooked errands</h2>
+
       {!isLoading ? (
         <ul className='d-flex flex-wrap justify-content-center'>
-          {data.map(errand => {
+          {data.filter(errand => !errand.assignedUser).map(errand => {
             return (
               <ErrandCard
                 key={errand._id}
@@ -40,10 +42,39 @@ const UserProfile = () => {
                 category={errand.category}
                 image={errand.imageUrl}
                 user={errand.user}
+                assignedUser={errand.assignedUser}
                 refreshMain={refreshMain}
               />
             );
           })}
+        </ul>
+      ) : (<p>Loading...</p>)}
+
+      <h2 className='px-4'>Booked errands</h2>
+
+      {!isLoading ? (
+        <ul className='d-flex flex-wrap justify-content-center'>
+          {data.filter(errand => errand.assignedUser).map(errand => {
+            return (
+              <ErrandCard
+                key={errand._id}
+                id={errand._id}
+                title={errand.title}
+                description={errand.description}
+                location={errand.location}
+                compensation={errand.compensation}
+                dateDue={errand.dateDue}
+                timeDue={errand.timeDue}
+                category={errand.category}
+                image={errand.imageUrl}
+                user={errand.user}
+                assignedUser={errand.assignedUser}
+                refreshMain={refreshMain}
+              />
+            );
+            // show errands by booked
+          })}
+          
         </ul>
       ) : (<p>Loading...</p>)}
     </Fragment>
